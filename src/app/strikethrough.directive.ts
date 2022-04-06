f7aece7363bbdde93b2e45cf64d49aee74e8879f
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-06 14:07:17 +0300

    User-initiated events

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -5,8 +5,10 @@ import { Directive,ElementRef } from '@angular/core';
 })
 export class StrikethroughDirective {
 
-  constructor(private elem:ElementRef) {
-    this.elem.nativeElement.style.textDecoration='line-through';
+  constructor(private elem:ElementRef) {}
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
    }
 
 }
